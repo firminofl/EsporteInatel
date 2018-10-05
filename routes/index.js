33c1744e8a7df6c9bc1dd9ba6487d618b85a6010
firminofl <30608181+firminofl@users.noreply.github.com>
2018-10-04 22:26:13 -0300

    Firebase estavel durante as operacoes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,17 @@ var router = express.Router();
 var pessoa =  [];
 var firebase = require("firebase");
 
+
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyCWmja0zi6SBxOV4vOq5Exm0nA_ZEGoxz8",
+authDomain: "esporteinatel.firebaseapp.com",
+databaseURL: "https://esporteinatel.firebaseio.com",
+projectId: "esporteinatel",
+storageBucket: "esporteinatel.appspot.com",
+messagingSenderId: "579754760416"
+};
+
 /* GET home page. */
 router.get('/', function(request, response, next) {
   response.render('index', { title: 'Inicial' , pessoa: pessoa });
@@ -31,36 +42,21 @@ router.post('/cadastrar-pessoa', function(request, response, next) {
 	console.log("--------------");
 
   response.render('index', { title: 'Cadastrar Pessoa', pessoa: pessoa });
+
+  next();
 });
 
 function writeUserData(nome, email, data, cpf) {
-	var contador = 0;
-
-	if(contador == 0){
-
-		initializeFirebase();
-
+	if (!firebase.apps.length) {
+		firebase.initializeApp(config);
+	}else{
 		firebase.database().ref('/'+cpf).set({
-	    	nome: nome,
-	    	email: email,
-	    	data: data,
-	    	cpf: cpf
+			nome: nome,
+			email: email,
+			data: data,
+			cpf: cpf
 		});
-		contador = 1;
 	}
-}
-
-function initializeFirebase(){
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCWmja0zi6SBxOV4vOq5Exm0nA_ZEGoxz8",
-    authDomain: "esporteinatel.firebaseapp.com",
-    databaseURL: "https://esporteinatel.firebaseio.com",
-    projectId: "esporteinatel",
-    storageBucket: "esporteinatel.appspot.com",
-    messagingSenderId: "579754760416"
-  };
-  firebase.initializeApp(config);
 }
 
 module.exports = router;
